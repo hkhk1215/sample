f89d55e9ffe63c102b676d5ecfa5bef3da90f78c
harish <karthickharish@gmail.com>
2021-09-21 22:25:36 -0700

    UP

--- src/App.js
+++ src/App.js
@@ -5,32 +5,18 @@ import { NxApiService } from 'axit-new-test-1';
 import Page from './page';
 import { useEffect } from 'react';
 
-const envs = process.env.react_app_env;
+const envs = process.env.react_app_baseurl;
 function App() {
   
   useEffect(() => {
     console.log(envs)
-    NxApiService.CONFIG(envs)
+    // NxApiService.CONFIG(envs)
   },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <BrowserRouter>
       <Switch>
-      <Route path='/page'  component={Page}>
+      <Route path='/'  component={Page}>
         
       </Route>
       </Switch>
--- src/page.js
+++ src/page.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NxApiService } from 'axit-new-test-1';
 
 function Page() {
+    const [data, setData] = useState({
+        userName: '',
+        password : "",
+    })
     return (
         <div>
+            <input onChange={(e) => {
+                setData({...data, userName: e.target.value})
+            }} value={data.userName} />
+            <input onChange={(e) => {
+                setData({...data, password: e.target.value})
+            }} value={data.password} />
             <button onClick={() => {
-                NxApiService.POST({
-                    url : 'login',
-                    data : {
-                        email : 'h@mail.com',
-                        password: '123'
-                    }
-                })
+                
             }}>GET</button>
             <button>Put</button>
         </div>
